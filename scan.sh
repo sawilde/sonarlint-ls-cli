771b417f424d8a4bc216683aa5678b19dcdf4d53
Vincent FENET <vincent@fenet.fr>
2025-03-28 13:52:41 +0100

    checkout project if needed

--- scan.sh
+++ scan.sh
@@ -13,4 +13,13 @@ if [ ! -e ~/.sonarlint-ls/venv ]; then
     python3 -m venv ~/.sonarlint-ls/venv
     ~/.sonarlint-ls/venv/bin/pip install -r requirements.txt
 fi
-~/.sonarlint-ls/venv/bin/python scan.py --sonarlint-ls ~/.sonarlint-ls/server/*.jar --analyzers ~/.sonarlint-ls/analyzers/*.jar -- $*
+DIR=$(dirname "$0")
+if [ -e $DIR/scan.py ]; then
+    SCAN=$DIR/scan.py
+else
+    if [ ! -e ~/.sonarlint-ls/sonarlint-ls-cli ]; then
+        git clone https://github.com/vincentfenet/sonarlint-ls-cli ~/.sonarlint-ls/sonarlint-ls-cli
+    fi
+    SCAN=~/.sonarlint-ls/sonarlint-ls-cli/scan.py
+fi
+~/.sonarlint-ls/venv/bin/python $SCAN --sonarlint-ls ~/.sonarlint-ls/server/*.jar --analyzers ~/.sonarlint-ls/analyzers/*.jar -- $*
